a806ad8e6122182627d438318e8a03cd5d5b83b7
kingspp <kingspprathyush@gmail.com>
2015-06-02 14:09:17 +0530

    Added IP Settings

--- src/com/example/piscope_android/SettingsActivity.java
+++ src/com/example/piscope_android/SettingsActivity.java
@@ -1,16 +1,80 @@
 package com.example.piscope_android;
 
+
+
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SettingsActivity extends ActionBarActivity {
-
+	
+	TextView ipaddr;
+	String url="";
+	Button ipsave;
+	final Context context = this;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		
+		ipaddr= (TextView) findViewById(R.id.textView3);
+		ipaddr.setText(url);
+		ipsave = (Button)findViewById(R.id.button1);
+		ipsave.setOnClickListener(new View.OnClickListener() {
+	        @Override
+	        //On click function
+	        public void onClick(View view) {
+	        	/* Alert Dialog Code Start*/ 	
+	        	AlertDialog.Builder alert = new AlertDialog.Builder(context);
+	        	alert.setTitle("Raspberry Pi IP Address"); //Set Alert dialog title here
+	        	alert.setMessage("Please enter Raspberry Pi IP Address\n"); //Message here
+
+	            // Set an EditText view to get user input 
+	            final EditText input = new EditText(context);
+	            alert.setView(input);
+
+	        	alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+	        	public void onClick(DialogInterface dialog, int whichButton) {
+	        	 //You will get as string input data in this variable.
+	        	 // here we convert the input to a string and show in a toast.
+	        	 url = input.getEditableText().toString();
+	        	 Toast.makeText(context,"IP address changed to"+url,Toast.LENGTH_LONG).show(); 
+	        	 writeURL(url);
+	        	 ipaddr.setText(url);
+	        	} // End of onClick(DialogInterface dialog, int whichButton)
+
+				private void writeURL(String url) {
+					// TODO Auto-generated method stub
+					SharedPreferences prefs = getSharedPreferences(
+							"Preferences.xml", MODE_PRIVATE);
+					SharedPreferences.Editor editor = prefs.edit();
+					editor.putString("ip", url);
+					editor.commit(); //
+					
+				}
+	        }); //End of alert.setPositiveButton
+	        	alert.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+	        	  public void onClick(DialogInterface dialog, int whichButton) {
+	        	    // Canceled.
+	        		  dialog.cancel();
+	        	  }
+	        }); //End of alert.setNegativeButton
+	        	AlertDialog alertDialog = alert.create();
+	        	alertDialog.show();
+	       /* Alert Dialog Code End*/ 
+	        }
+	    });
 	}
 
 	@Override
