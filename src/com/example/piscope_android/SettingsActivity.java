66d298670c3bdc7595c971e8f962fdf5c43519e9
kingspp <kingspprathyush@gmail.com>
2015-06-02 13:21:11 +0530

    Added Settings

--- src/com/example/piscope_android/SettingsActivity.java
+++ src/com/example/piscope_android/SettingsActivity.java
@@ -1,26 +1,22 @@
 package com.example.piscope_android;
 
-
-
 import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class SettingsActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_settings);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
 	}
 
@@ -31,8 +27,6 @@ public class MainActivity extends ActionBarActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-			Intent i = new Intent(this, SettingsActivity.class);
-			startActivity(i);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
