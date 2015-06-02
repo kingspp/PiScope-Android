b06d2f30c4dee90c5cd1fe08916c6e5cc0b6a014
kingspp <kingspprathyush@gmail.com>
2015-06-02 13:42:06 +0530

    Fixed Internet Permission issues in Manifest File

--- src/com/example/piscope_android/MainActivity.java
+++ src/com/example/piscope_android/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends ActionBarActivity {
 								"Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19");
 				browser.setWebViewClient(new MyBrowser());
 				browser.setWebViewClient(new MyWebViewClient());
-				browser.loadUrl("http://" + url);
+				browser.loadUrl(url);
 	}
 	
 	private boolean isNetworkAvailable() {
